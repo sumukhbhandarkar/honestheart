44111db93ecccd92a9725d0aa81ca60206a08054
Sumukh Bhandarkar <sumukhbhandarkar@gmail.com>
2025-10-22 15:46:15 +0530

    first commit

---
+++ src/main/java/com/sumukhbhandarkar/honestheart/HonestheartApplication.java
@@ -0,0 +1,13 @@
+package com.sumukhbhandarkar.honestheart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonestheartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonestheartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumukhbhandarkar/honestheart/HonestheartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumukhbhandarkar.honestheart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonestheartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
